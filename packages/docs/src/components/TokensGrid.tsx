e3a5cb11166f5126f53f8b267cc01999828e625b
lucsa <seyadeodin@gmail.com>
2022-10-23 19:13:39 -0300

    added tokens grid

--- packages/docs/src/components/TokensGrid.tsx
+++ packages/docs/src/components/TokensGrid.tsx
@@ -1,4 +1,8 @@
-export function TokensGrid() {
+interface TokensGridProps {
+  tokens: Record<string, string>
+}
+
+export function TokensGrid({ tokens }: TokensGridProps) {
   return (
     <table>
       <thead>
@@ -7,6 +11,17 @@ export function TokensGrid() {
           <th>Value</th>
         </tr>
       </thead>
+
+      <tbody>
+        {Object.entries(tokens).map(([key, value]) => {
+          return (
+            <tr key={key}>
+              <td>{key}</td>
+              <td>{value}</td>
+            </tr>
+          )
+        })}
+      </tbody>
     </table>
   )
 }
